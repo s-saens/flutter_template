ed0c07de3d39e5c149399d3cd260dfe594c87a3c
saens <songsh5775@gmail.com>
2023-08-25 16:47:52 +0900

    rename home

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_template/screens/home.dart';
+import 'package:flutter_template/screens/s_home.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
